ed44e4f4dd4ba05b524249844409f06e69188f0a
jexhsu <jexhsu@gmail.com>
2024-06-15 07:00:31 +0800

    controlled inputs(forms)

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type='text'
+          placeholder='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          placeholder='body'
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value='mario'>mario</option>
+          <option value='yoshi'>yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
