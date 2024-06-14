042d1e07ecbdf23c7c95178f926c238def6171dc
jexhsu <jexhsu@gmail.com>
2024-06-14 15:11:30 +0800

    fetching data with useEffect && add favico

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,7 +1,6 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs }) => {
   return (
     <div className='blog-list'>
-      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div
           className='blog-preview'
@@ -9,7 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         >
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
