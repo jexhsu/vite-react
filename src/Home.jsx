663a3498a8ca17c61b4f8da065923e5dcea0bfc4
jexhsu <jexhsu@gmail.com>
2024-06-14 14:28:03 +0800

    useEffect dependencies

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,6 +13,8 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -21,11 +23,12 @@ const Home = () => {
   useEffect(() => {
     console.log("use effect ran");
     console.log(blogs);
-  });
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("luigi")}>change name</button>
     </div>
   );
 };
