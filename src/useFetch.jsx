3b7e2e34b32a9c9eb999b00067c494d7ae3620d9
jexhsu <jexhsu@gmail.com>
2024-06-14 20:50:09 +0800

    useEffect cleanup(optional)

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -6,10 +6,13 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
+            // error coming back from server
             throw Error('could not fetch the data for that resource');
           }
           return res.json();
@@ -17,12 +20,21 @@ const useFetch = (url) => {
         .then((data) => {
           setIsPending(false);
           setData(data);
+          setError(null);
         })
         .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          }
         });
-    }, 500);
+    }, 1000);
+
+    // abort the fetch
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, isPending, error };
