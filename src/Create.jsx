03931fa4e78c0895e0b141e6fd002eadf3c1f133
jexhsu <jexhsu@gmail.com>
2024-06-15 08:15:49 +0800

    programmatic redirects

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSumit = (e) => {
     e.preventDefault();
@@ -17,6 +19,7 @@ const Create = () => {
     }).then(() => {
       console.log('new blog added');
       setIsPending(false);
+      navigate('/');
     });
   };
 
