4691d5524d84b9c6ba7d86c3742aafa2cf891878
jexhsu <jexhsu@gmail.com>
2024-07-21 14:26:57 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const domNood = document.getElementById('root');
+const root = createRoot(domNood);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
